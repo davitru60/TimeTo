4eba1929bc479fc3e96078c17392e2022bdc66aa
davitru60 <davitru60@gmail.com>
2024-06-01 21:36:33 +0200

    act

--- Frontend/src/app/pages/home/home.component.ts
+++ Frontend/src/app/pages/home/home.component.ts
@@ -1,33 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener} from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../../shared/components/layout/navbar/navbar.component';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss',
-    imports: [CommonModule, RouterLink, NavbarComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  imports: [CommonModule, RouterLink, NavbarComponent],
 })
 export class HomeComponent {
-    isSmallScreen = false;
-    isMediumScreen = false;
-    isLargeScreen = false;
-  
-    @HostListener('window:resize', ['$event'])
-    onResize(event: any) {
-      this.checkScreenSize();
-    }
-  
-    constructor() {
-      this.checkScreenSize();
-    }
-  
-    private checkScreenSize() {
-      const width = window.innerWidth;
-      this.isSmallScreen = width < 768;
-      this.isMediumScreen = width >= 768 && width < 1280;
-      this.isLargeScreen = width >= 1260;
-    }
+  isSmallScreen = false;
+  isMediumScreen = false;
+  isLargeScreen = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
   }
+
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    const width = window.innerWidth;
+    this.isSmallScreen = width < 768;
+    this.isMediumScreen = width >= 768 && width < 1280;
+    this.isLargeScreen = width >= 1260;
+  }
+
+  selectedContent: string = 'estrategia';
+
+  selectContent(content: string) {
+    this.selectedContent = content;
+  }
+}
